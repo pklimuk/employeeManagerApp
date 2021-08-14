5e2ce6f087d59177b35b9dc20a16de1e1e6a4889
Pavel Klimuk <pavelklimuk@yahoo.com>
2021-08-14 11:44:54 +0200

    Initial commit

---
+++ src/test/java/com/pklimuk/employeemanagerapp/EmployeeManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pklimuk.employeemanagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
